c6349cbce2274dc7351be05216a067284c7ae8a0
Daniel Temalatzi Mojica <danxvv@gmail.com>
2021-05-18 10:12:30 -0500

    edited main.py

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ app.register_blueprint(estados_BP)
 app.register_blueprint(colonia_BP)
 app.register_blueprint(municipio_BP)
 
+@app.route("/api")
+def welcome():
+    return """
+        <h1>END POINTS</h1>
+        
+        <p>/api/cp - Nos devuelve todos los codigos postales del pais (tarda un poco en cargar todos)</p>
+        <p>/api/cp/{numerocp} - Al poner un codigo postal valido, nos devolvera Codigo Postal - Nombre Colonia - Nombre Estado - Tipo Colonia - Tipo Zona</p>
+        <p>/api/estados - Devuelve los estados con su numero de estado y su capital</p>
+        <p>/api/estado/{estado} - Busca en los estados si hay alguno que coincida con el endpoint</p>
+        <p>/api/municipios - Devuelve los municipios del pais y el estado al que pertencen</p>
+        <p>/api/municipio/{municipio} Busca en los municipios si hay alguno que coincida con el endpoint</p>
+        <p>/api/colonias/ Devuelve todas las colonias del pais</p>
+        <p>/api/colonia/{colonia} Busca en las colonias si hay alguno que coincida con el endpoint</p>
+        
+    """
 
 @app.after_request
 def after_request(response):
